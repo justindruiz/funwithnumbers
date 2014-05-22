dc881c9117fd4ad783bde4237b6656967ee8bc7a
justindruiz <justinruizwrestling@gmail.com>
2014-05-22 16:04:53 -0600

    updated fun_with_numbers.rb

--- fun_with_numbers.rb
+++ fun_with_numbers.rb
@@ -7,3 +7,12 @@ puts "What is your second favorite number?"
 number2 = gets.to_i
 
 puts "If your favorite numbers got together, the total would be #{number1 + number2}."
+
+total = number1 + number2
+
+if total.even?
+  puts "Your number is even."
+
+else total.odd?
+  puts "Your number is odd."
+end
