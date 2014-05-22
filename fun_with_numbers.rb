35b5a57e8ff58dda2c28df711137240277db0104
justindruiz <justinruizwrestling@gmail.com>
2014-05-22 15:46:25 -0600

    added file fun_with_numbers.rb

---
+++ fun_with_numbers.rb
@@ -0,0 +1,9 @@
+puts "What is your favorite number?"
+
+number1 = gets.to_i
+
+puts "What is your second favorite number?"
+
+number2 = gets.to_i
+
+puts "If your favorite numbers got together, the total would be #{number1 + number2}."
